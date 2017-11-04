1db9bbf7cdf18d8bb59fae02a053faa52f249140
mkkemp94 <mkkemp94@gmail.com>
2017-11-04 11:51:27 -0400

    Win Lose and Level01 Scenes, Transition, and Music

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	static MusicPlayer instance = null;
+	
+	// Use this for initialization
+	void Start () {
+		if (instance == null) {
+			GameObject.DontDestroyOnLoad(gameObject);
+			instance = this;
+		} else {
+			Destroy(gameObject);
+			print ("Self-destructing duplicate music player!");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
