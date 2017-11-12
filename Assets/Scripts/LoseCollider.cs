b565dd0a7b9261beae732e0f5c9fd5495d3d4f4d
mkkemp94 <mkkemp94@gmail.com>
2017-11-12 09:41:32 -0500

    Add ball and collider

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D trigger) {
+		print ("Trigger");
+	}
+	
+	void OnTriggerEnter2D (Collision2D collision) {
+		print ("Collision");
+	}
+}
