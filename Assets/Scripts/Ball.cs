1675fdcf206de1e8cc66605c13d4754e2b834148
mkkemp94 <mkkemp94@gmail.com>
2017-11-15 20:55:05 -0500

    Ball sticks to paddle and moves with paddle before launch.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public Paddle paddle;
+	
+	private Vector3 paddleToBallVector;
+	
+	// Use this for initialization
+	void Start () {
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = paddle.transform.position + paddleToBallVector;
+	}
+}
